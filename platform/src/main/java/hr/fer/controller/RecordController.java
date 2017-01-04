e47783211c71d176a26ef4431aa59bb78511326e
Domagoj Hrestak <domagoj.hrestak@gmail.com>
2017-01-04 12:18:33 +0100

    zapoceti recordwebcontroller

--- platform/src/main/java/hr/fer/controller/RecordController.java
+++ platform/src/main/java/hr/fer/controller/RecordController.java
@@ -4,6 +4,8 @@ import hr.fer.model.Record;
 import hr.fer.service.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -45,5 +47,7 @@ public class RecordController {
     public void deleteRecord(@PathVariable Integer id) {
         storageService.deleteRecords(id);
     }
+
+
 
 }
--- platform/src/main/java/hr/fer/controller/web/RecordWebController.java
+++ platform/src/main/java/hr/fer/controller/web/RecordWebController.java
@@ -1,41 +1,31 @@
 package hr.fer.controller.web;
 
-import hr.fer.controller.web.viewbags.UserSensorsBag;
-import hr.fer.model.Record;
-import hr.fer.service.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import hr.fer.service.StorageService;
+import org.springframework.ui.Model;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
-import java.util.ArrayList;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
-//@RequestMapping("/records")
-//public class RecordWebController {
-//
-//    private final StorageService storageService;
-//
-//    @Autowired
-//    public RecordWebController(StorageService storageService) {
-//        this.storageService = storageService;
-//    }
-//
-//    @GetMapping
-//    public String createIndex(ModelMap model) {
-//        model.addAttribute("userSensorsBag", new UserSensorsBag());
-//        return "record/index";
-//    }
-//
-//    @PostMapping
-//    public Iterable<Record> readRecords(ModelMap modelMap) {
-//        UserSensorsBag userSensorsBag = (UserSensorsBag) modelMap.getOrDefault("userSensorsBag", null);
-//        if (userSensorsBag != null) {
-//            return storageService.readRecords(userSensorsBag.getUserId(),
-//                    userSensorsBag.getSensorIds().stream().filter(Objects::nonNull).collect(Collectors.toList()));
-//        }
-//        return new ArrayList<>();
-//    }
-//}
+@Controller
+@RequestMapping("/listRecords")
+public class RecordWebController {
+
+    private final StorageService storageService;
+
+    @Autowired
+    public RecordWebController(StorageService storageService) {
+        this.storageService = storageService;
+    }
+
+    @RequestMapping(method = {GET, POST})
+    public String getRecords(Model model) {
+
+        model.addAttribute("records", storageService.getAllRecords());
+        return "record/record";
+    }
+
+
+}
